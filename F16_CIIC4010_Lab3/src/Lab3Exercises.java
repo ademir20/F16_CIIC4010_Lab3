770a67bf8a49a181ace708add36764032c5d9a33
4015b10 <4015b10@136.145.57.226>
2016-08-31 11:00:45 -0400

    Adjustments made to show my name and move window to the center.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Ademir Khattar Jbour");
+		JFrame myFrame = new JFrame("Ademir Khattar Jbour Sanchez");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(850, 450);
 		myFrame.setSize(200, 200);
